fe654bfb4f8ba45b7edb7883676183dc201ec02d
Rocket-Octane <93807708+Rocket-Octane@users.noreply.github.com>
2022-05-12 22:53:13 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ Webcam.set({
 
 camera = document.getElementById("camera");
 
-Webcam.attach("#camera");
+Webcam.attach('#camera');
 
 function take_snapshot(){
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML = "<img id='captured image' src"+data_uri+">";
+        document.getElementById("result").innerHTML = "<img id='captured_image' src"+data_uri+">";
     });
 }
 
@@ -21,4 +21,21 @@ var classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/mo
 
 function modelLoaded(){
     console.log("Model Loaded");
+}
+
+function check(){
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        var x = results[0].confidence.toFixed(4);
+        document.getElementById("result_accuracy_name").innerHTML = x * 100 + "%";
+    }
 }
